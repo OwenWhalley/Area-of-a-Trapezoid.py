f863a7d15ba630d5f547352c48872f7cdef4fbc2
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-09-29 09:59:20 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Calculate the Area of a Trapezoid")
+Base_A = input("Enter Base A (cm)")
+Base_B = input("Enter Base B (cm)")
+Height = input("Enter Height (cm)")
+Base_A_B = Base_A + Base_B
+Base_A_B_2 = Base_A_B / int(2)
+Final = Base_A_B_2 * Height
+print("The Area of the Trapezoid is")("Final")
